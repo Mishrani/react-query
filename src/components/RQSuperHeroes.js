276bed8edee352198a1bc9f121b84b7c824f23ba
mishrani <mishra.nitesh987@gmail.com>
2024-01-01 20:15:41 +0530

    data trandformation

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -9,14 +9,15 @@ const fetchSuperHeroes = async () => {
 const RQSuperHeroes = () => {
   const onSuccess = (data) => {
     console.log('Success', data);
-  }
+  };
   const onError = (error) => {
     console.log('Error', error);
-  }
+  };
+
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'superheroes',
     fetchSuperHeroes,
-    { 
+    {
       // cacheTime: 5000
       // staleTime: 30000
       // refetchOnMount: true/false/always
@@ -24,12 +25,18 @@ const RQSuperHeroes = () => {
       // refetchInterval: false/value in milliseconds
       //refetchInteralInBackground
       // enabled: false,
-      onSuccess: onSuccess,
-      onError: onError
+      onSuccess,
+      onError,
+      select: (data) => {
+        const superHeroNames = data.map((hero) => hero.name);
+        console.log('superHeroNames', superHeroNames);
+        return superHeroNames;
+      }
     }
   );
 
-  console.log({ isLoading, isFetching });
+  // console.log({ isLoading, isFetching });
+
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
@@ -37,15 +44,19 @@ const RQSuperHeroes = () => {
   if (isError) {
     return <h2>{error.message}</h2>;
   }
-
+  console.log('DATA', data);
   return (
     <>
-    {/* <button onClick={refetch}>Fetch</button> */}
+      {/* <button onClick={refetch}>Fetch</button> */}
       <h2>RQ Super Heroes</h2>
 
-        {data && data.map((hero) => (
+      {/* {data && data.map((hero) => (
           <div key={hero.name}>{hero.name}</div>
-        ))}
+        ))} */}
+      {data &&
+        data?.map((heroName) => {
+          return <div key={heroName}>{heroName}</div>;
+        })}
     </>
   );
 };
