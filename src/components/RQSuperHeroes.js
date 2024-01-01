14b56789a0c0030d0f845a475ab103a14350340f
mishrani <mishra.nitesh987@gmail.com>
2024-01-01 20:27:22 +0530

    use a custom hook

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -1,10 +1,4 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
-
-const fetchSuperHeroes = async () => {
-  const response = await axios.get('http://localhost:4000/superheroes');
-  return response?.data;
-};
+import { useSuperHeroesData } from '../hooks/useSuperHeroesData';
 
 const RQSuperHeroes = () => {
   const onSuccess = (data) => {
@@ -14,26 +8,8 @@ const RQSuperHeroes = () => {
     console.log('Error', error);
   };
 
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    'superheroes',
-    fetchSuperHeroes,
-    {
-      // cacheTime: 5000
-      // staleTime: 30000
-      // refetchOnMount: true/false/always
-      // refetchOnWindowFocus: true/false/always
-      // refetchInterval: false/value in milliseconds
-      //refetchInteralInBackground
-      // enabled: false,
-      onSuccess,
-      onError,
-      select: (data) => {
-        const superHeroNames = data.map((hero) => hero.name);
-        console.log('superHeroNames', superHeroNames);
-        return superHeroNames;
-      }
-    }
-  );
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
   // console.log({ isLoading, isFetching });
 
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = async () => {
+  const response = await axios.get('http://localhost:4000/superheroes');
+  return response?.data;
+};
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+  return useQuery('superheroes', fetchSuperHeroes, {
+    // cacheTime: 5000
+    // staleTime: 30000
+    // refetchOnMount: true/false/always
+    // refetchOnWindowFocus: true/false/always
+    // refetchInterval: false/value in milliseconds
+    //refetchInteralInBackground
+    // enabled: false,
+    onSuccess,
+    onError,
+    select: (data) => {
+      const superHeroNames = data.map((hero) => hero.name);
+      console.log('superHeroNames', superHeroNames);
+      return superHeroNames;
+    }
+  });
+};
